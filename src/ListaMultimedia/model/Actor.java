ff39d732f83ea1dbb06df9e445e288d72d6cd70c
mariothegreek <147819942+mariothegreek@users.noreply.github.com>
2024-03-17 14:12:29 +0100

    ElementosMultimedia (funcional working)

---
+++ src/ListaMultimedia/model/Actor.java
@@ -0,0 +1,11 @@
+package ListaMultimedia.model;
+
+public class Actor extends Persona {
+    public Actor() {
+    }
+
+    public Actor(String nombre, String dni) {
+        super(nombre, dni);
+    }
+
+}
